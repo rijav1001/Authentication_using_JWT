d118fe63ff5a860d2bada0d71b1a68739e46126c
rijav1001 <rvu100199@gmail.com>
2022-01-17 19:31:21 +0530

    created display of cities

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,21 +1,33 @@
-import { Route, Routes, NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import LoginForm from './components/LoginForm';
-import Register from './components/Register';
 
 function App() {
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/cities')
+    .then((res) => res.json())
+    .then((res) => setCities(res))
+    .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
-     <header>
-       <nav className="nav-tab">
-        <NavLink to="/login"><button className='nav-to'>Login</button></NavLink>
-        <NavLink to="/register"><button className="nav-to">Register</button></NavLink>
-       </nav>
-     </header>
-        <Routes>
-            <Route exact path='/login' element={<LoginForm/>}></Route>
-            <Route exact path='/register' element={<Register/>}></Route>
-        </Routes>
+      <div className='Container'>
+        <h2>List of Cities</h2>
+        <div className='sub-content'>
+          {
+            cities.map((item, idx) => {
+              return (
+                <div className='city' key={idx}>
+                  <p>{item.name}</p>
+                  <p>{item.state}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </>
   );
 }
